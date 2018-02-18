1be96dd6cc8f3cdd8f6ba35be7846fd75644f0f1
Alexander Chin <alexander.chin@stonybrook.edu>
2018-02-17 21:48:59 -0500

    Add files via upload

--- nwh.py
+++ nwh.py
@@ -1,17 +1,57 @@
-import requests, json
+from flask import Flask, render_template
+import requests, json, urllib
+import xml.etree.ElementTree as etree
 
-url = 'https://apim-hie-dev.azure-api.net/restsda/patient/SDA'
-data = {
-    'AssigningAuthority':'NSMG',
-    'MRN' : '541276'
- }
-headers = {
-    # Request headers
-    'Requestor': 'request',
-    # 'Content-Type': 'application/json',
-    'Content-Type': 'application/json',
-    'Ocp-Apim-Subscription-Key': 'ad108eefd75549898aac1908c3cd9265',
-}
-r = requests.post(url, data=json.dumps(data), headers=headers)
-response = r.text
-print(response)
+
+# print(data
+
+drug = 'clindamycin'
+url='https://rxnav.nlm.nih.gov/REST/drugs?name='
+drugUrl = url + drug
+xml = requests.get(drugUrl).content
+
+
+with open('data.xml', 'wb') as outfile:
+    outfile.write(xml)
+
+tree = etree.parse('data.xml')
+root = tree.getroot()
+
+# for child in root:
+#     print (child.tag, child.attrib)
+
+def find(str):
+	syn_list = []
+	path = './/' + str
+	for med in root.findall(path):
+		tag = etree.tostring(med)
+		item = etree.fromstring(tag)
+		string =''.join(item.itertext())
+		string =string.partition(' ')[0]
+		while string not in syn_list and string != '' and not (string.isdigit()):
+			syn_list.append(string)
+	print(syn_list)
+	return
+
+find('synonym')
+
+
+app = Flask(__name__)
+ 
+@app.route("/")
+def index():
+	return render_template('index.html')
+
+@app.route('/', methods=['POST'])
+def welcome_page():
+	return render_template("welcome.html")
+
+@app.route('/welcome', methods =['POST'])
+def get_drugs():
+	drug = request.form(drug)
+	url='https://rxnav.nlm.nih.gov/REST/drugs?'
+	newUrl = url + drug
+	data = json.loads(request.get(newUrl).json)
+
+if __name__ == "__main__":
+    app.run()
